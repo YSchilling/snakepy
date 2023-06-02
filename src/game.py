f705a38da04598d2829ee0de8ca3d116c097ed40
Pandakie <78358953+YSchilling@users.noreply.github.com>
2023-06-02 09:21:30 +0200

    added cells for sizing and step movement

--- src/game.py
+++ src/game.py
@@ -4,17 +4,19 @@ from snake import Snake
 class Game:
     def __init__(self):
         # settings
-        WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
-        self.FPS = 60
+        WINDOW_WIDTH_AND_HIGHT = 600
+        self.FPS = 5
         self.BACKGROUND_COLOR = (30, 30, 30)
+        self.cell_amount = 20
         
         # init window
-        self.WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.WINDOW = pygame.display.set_mode((WINDOW_WIDTH_AND_HIGHT, WINDOW_WIDTH_AND_HIGHT))
         pygame.display.set_caption("Snakepy")
 
         # init game
+        self.cell_size = self.WINDOW.get_width() / self.cell_amount
         self.CLOCK = pygame.time.Clock()
-        self.snake = Snake()
+        self.snake = Snake(self)
 
     def run(self):
         self.CLOCK.tick(self.FPS)
--- src/snake.py
+++ src/snake.py
@@ -3,9 +3,14 @@ from snake_head import SnakeHead
 from direction import Direction
 
 class Snake:
-    def __init__(self):
+    def __init__(self, game):
+        self.game = game
+        
         self.head_group = pygame.sprite.GroupSingle()
-        self.head_group.add(SnakeHead((20, 20), (200, 200), (0, 255, 0)))
+        size = (game.cell_size, game.cell_size)
+        middle_coords = game.cell_size * (game.cell_amount / 2)
+        pos = (middle_coords, middle_coords)
+        self.head_group.add(SnakeHead(size, pos, (0, 255, 0)))
     
     def update(self):
         self.head_group.update()
@@ -18,10 +23,10 @@ class Snake:
         snake_head = self.head_group.sprite
 
         if snake_head.direction == Direction.UP:
-            snake_head.rect.y -= 1
+            snake_head.rect.y -= self.game.cell_size
         elif snake_head.direction == Direction.RIGHT:
-            snake_head.rect.x += 1
+            snake_head.rect.x += self.game.cell_size
         elif snake_head.direction == Direction.DOWN:
-            snake_head.rect.y += 1
+            snake_head.rect.y += self.game.cell_size
         elif snake_head.direction == Direction.LEFT:
-            snake_head.rect.x -= 1
+            snake_head.rect.x -= self.game.cell_size
