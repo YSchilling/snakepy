c8be427bc5dadf6024febfba8e762ae7de47d1c7
YSchilling <78358953+YSchilling@users.noreply.github.com>
2023-06-06 18:56:00 +0200

    added fruit spawning

---
+++ src/fruit.py
@@ -0,0 +1,10 @@
+import pygame
+
+class Fruit(pygame.sprite.Sprite):
+    def __init__(self, size, pos, color):
+        super().__init__()
+
+        self.image = pygame.surface.Surface(size)
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
--- src/game.py
+++ src/game.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from snake import Snake
+from fruit import Fruit
 
 class Game:
     def __init__(self):
@@ -18,9 +20,12 @@ class Game:
         # init game
         self.cell_size = self.WINDOW.get_width() / self.cell_amount
         self.CLOCK = pygame.time.Clock()
-        self.snake = Snake(self)
         self.delta_time = 0
         self.running = True
+        self.snake = Snake(self)
+
+        self.fruit_group = pygame.sprite.GroupSingle()
+        self.spawn_fruit()
 
     def run(self):
         self.delta_time = self.CLOCK.tick(self.FPS)
@@ -38,3 +43,22 @@ class Game:
         self.snake.draw(self.WINDOW)
 
         pygame.display.flip()
+    
+    def _spawn_fruit(self):
+
+        found_empty_cell = False
+        while not found_empty_cell:
+            pos_x_list = [x for x in range(0, self.WINDOW.get_width(), int(self.cell_size))]
+            pos_y_list = [y for y in range(0, self.WINDOW.get_height(), int(self.cell_size))]
+
+            pos_x = random.choice(pos_x_list)
+            pos_y = random.choice(pos_y_list)
+
+            test_rect = pygame.rect.Rect((pos_x, pos_y), self.cell_size)
+            if (
+                test_rect.collidelist(self.snake.tails_group.sprites()) == -1
+                and test_rect.collidelist(self.snake.head_group.sprites()) == -1
+            ):
+                found_empty_cell = True
+
+        self.fruit_group.add(Fruit(self.cell_size, pos_x, pos_y))
